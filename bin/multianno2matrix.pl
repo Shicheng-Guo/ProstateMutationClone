9a38c2bd851db7537eebf9a4103869083c2746e5
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-11 02:11:27 -0500

    Create multianno2matrix.pl

---
+++ bin/multianno2matrix.pl
@@ -0,0 +1,28 @@
+use strict;
+use List::Uniq ':all';
+my @file=glob("*hg19_multianno.txt");
+
+my %data;
+my %gene;
+my %sam;
+
+foreach my $file(@file){
+open F,$file;
+my($sam)=split/.hg19_multianno.txt/,$file;
+while(<F>){
+my @line=split/\s+/;
+$gene{$line[6]}=$line[6];
+$sam{$sam}=$sam;
+$data{$line[6]}{$sam}}++;
+}
+}
+
+my $sam=join("\t",sort keys %sam);
+print "\t$sam\n";
+foreach my $gene(sort keys %gene){
+print "$gene";
+foreach my $sam(sort keys %sam){
+print "\t$data{$gene}{$sam}";
+}
+print "\n";
+}
